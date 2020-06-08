dc91f255b75e3a82268ad1b7e3c6f52bd9a02b31
niv ben-porath <nbpapps@gmail.com>
2020-06-08 21:08:39 +0300

    working on it

--- TestMachine/Machine.swift
+++ TestMachine/Machine.swift
@@ -12,30 +12,39 @@ import Foundation
 class Machine {
     var isAvailable = true
     let id : String
+    private var queue = DispatchQueue(label: "machineQueue", attributes: .concurrent)
+
+    
+    
     init(id : String) {
         self.id = id
     }
     
-    func testToRun(_ test : Test, with completion : @escaping (String)-> Void) {
-       
+    func testToRun(_ test : Test, with completion : @escaping (String,String)-> Void) {
+        
         isAvailable = false//the machine is not available for tests
         
         print("Machine \(self.id) running test \(test.id)")
         
         //RUN TEST
         //here we added asyncAfter to simulate work done by the machine
-        DispatchQueue.main.asyncAfter(deadline: .now() + 1) { [weak self] in
+        
+        run(test: test) { [weak self] in
+            
             guard let self = self else { return }
             print("Machine \(String(describing: self.id)) finished test \(test.id)")
-            self.isAvailable = true // once work is done, the machine is available 
-            completion(test.id)//when we complete, we return the test id so we know this test has finished running
+            self.isAvailable = true // once work is done, the machine is available
+            completion(self.id,test.id)//when we complete, we return the test id so we know this test has finished running
         }
     }
     
-//    private func run(test : Test, with completion : @escaping ()->Void) {
-//        DispatchQueue.main.asyncAfter(deadline: .now() + 10) {
-//            completion()
-//        }
-//    }
+    private func run(test : Test, with completion : @escaping ()->Void) {
+        queue.async {
+//            DispatchQueue.main.asyncAfter(deadline: .now()) {
+                completion()
+//            }
+        }
+        
+    }
     
 }
--- TestMachine/Server.swift
+++ TestMachine/Server.swift
@@ -15,12 +15,16 @@ class Server {
     var runningTests = [Test]()
     var isActive = false
     
-//    init(machineArray : [Machine]) {
-//        self.machineArray = machineArray
-//    }
+    private var machineQueue = DispatchQueue(label: "machineArrayQueue", attributes: .concurrent)
+    private var testsQueue = DispatchQueue(label: "testsQueue", attributes: .concurrent)
+    private var runningTestsQueue = DispatchQueue(label: "runningTestsQueue", attributes: .concurrent)
+    
+    //    init(machineArray : [Machine]) {
+    //        self.machineArray = machineArray
+    //    }
     
     init() {
-//        machineArray  = [m1,m2,m3]
+        //        machineArray  = [m1,m2,m3]
     }
     
     //This is a handler that gets the tests that the server needs to run on machines
@@ -36,38 +40,68 @@ class Server {
         isActive = true
         //run test while we still have some in our list
         while listOfTests.count > 0 {
-            runTests()
+            self.runTests()
         }
     }
     
     private func runTests() {
         //find the first machine that is free
-        guard let freeMachine = machineArray.first(where: { (machine) -> Bool in
-            return machine.isAvailable
-        }) else {
+        guard let freeMachine = getFreeMachine() else {
             return
         }
         
         //make sure we have at least one test
-        guard let nextTest = listOfTests.first else {
+        guard let nextTest = getNextTest() else {
             return
         }
         
-        listOfTests.removeFirst()       //remove that test
-        runningTests.append(nextTest)   //and save it to the running list
-        
-        
         //call the test on the machine
-        freeMachine.testToRun(nextTest) { [weak self] (testId) in
+        freeMachine.testToRun(nextTest) { [weak self] (machineId, testId) in
             guard let self = self else {return}
             //when we complete, we find the first test that has the id of the test that was returned, and remove it from the running tests (if it failed, we could return it to the main list)
+            self.updateRun(for: testId)
+            
+        }
+    }
+    
+    
+    private func updateRun(for testId : String) {
+        machineQueue.sync(flags: .barrier) {
             if let finishedTestIndex = self.runningTests.firstIndex(where: { (test) -> Bool in
                 test.id == testId
             }) {
-                self.runningTests.remove(at: finishedTestIndex)
+                
+                self.runningTests.remove(at: finishedTestIndex) //remove test that has passed
+                //                let firstMachine = self.machineArray.removeFirst()
+                //                self.machineArray.append(firstMachine)
             }
         }
     }
+    
+    private func getFreeMachine() -> Machine? {
+        machineQueue.sync {
+            guard let freeMachine = machineArray.first(where: {$0.isAvailable}) else {
+                return nil
+            }
+            return freeMachine
+        }
+    }
+    
+    private func getNextTest() -> Test? {
+        testsQueue.sync(flags: .barrier) {
+            guard let nextTest = listOfTests.first else {
+                return nil
+            }
+            
+            listOfTests.removeFirst()       //remove that test
+            runningTestsQueue.sync(flags: .barrier) {
+                runningTests.append(nextTest)   //and save it to the running list
+                
+            }
+            return nextTest
+        }
+    }
+    
     
     
 }
